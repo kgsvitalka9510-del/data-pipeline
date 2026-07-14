9758e4eff79ec84ad413a94997586ae9ddbea843
Neptun <kgsvitalka9510@gmail.com>
2026-07-14 10:42:17 +0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data-pipeline",
+    version="1.0.0",
+    packages=find_packages(),
+    author="Developer",
+    description="Python data pipeline framework",
+    python_requires=">=3.8",
+)
